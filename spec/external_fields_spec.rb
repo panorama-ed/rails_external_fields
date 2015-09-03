580bc2cc894aa4d240fbf26fa2e8a7ff8b86d1a9
Sagar Jauhari <sagarjauhari@gmail.com>
2015-09-03 17:47:08 -0400

    All tests passing

--- spec/external_fields_spec.rb
+++ spec/external_fields_spec.rb
@@ -47,42 +47,43 @@ RSpec.describe ExternalFields, :temporal do
         expect(e.assoc.class).to eq(AssociationTestClass)
       end
 
-      it "should be saved when employee is saved" do
-        e = Fabricate(:employee)
-        expect(SIS::EmployeeData.count).to eq(0)
-        expect(e.extra_data.class).to eq(SIS::EmployeeData)
+      it "should be saved when the model is saved" do
+        e = TestClass.create!(name: "Hello")
+        expect(AssociationTestClass.count).to eq(0)
+        expect(e.assoc.class).to eq(AssociationTestClass)
         e.save!
-        expect(SIS::EmployeeData.count).to eq(1)
+        expect(AssociationTestClass.count).to eq(1)
       end
 
       it "should not be created or saved if unused" do
-        e = Fabricate(:employee)
-        e.first_name = "TEST"
+        e = TestClass.create!
+        e.name = "TEST"
         e.save!
-        expect(SIS::EmployeeData.count).to eq(0)
+        expect(AssociationTestClass.count).to eq(0)
       end
 
-      # This test is checking that student_friendly_name goes through
-      # extra_data.
       it "should be created if used" do
-        e = Fabricate(:employee, student_friendly_name: "TEST")
-        expect(SIS::EmployeeData.count).to eq(1)
-        expect(e.student_friendly_name).to eq "TEST"
+        e = TestClass.create!(name: "Hello", ext_field_1: "Field1")
+
+        expect(AssociationTestClass.count).to eq(1)
+        expect(e.ext_field_1).to eq "Field1"
       end
 
-      # This test also checks that student_friendly_name goes through
-      # extra_data.
-      it "should provide underscored methods" do
-        e = Fabricate(:employee, _student_friendly_name: "TEST")
-        expect(SIS::EmployeeData.count).to eq(1)
-        expect(e._student_friendly_name).to eq "TEST"
+      context "when underscore flag is true" do
+        it "should provide underscored methods" do
+          e = TestClass.create!(_ext_field_2: "_Field2")
+
+          expect(AssociationTestClass.count).to eq(1)
+          expect(e._ext_field_2).to eq "_Field2"
+        end
       end
 
       it "should provide an accessor that does not build a new object" do
-        e = Fabricate.build(:employee)
-        e.extra_data(use_original: true) # Access without building.
+        e = TestClass.new(name: "Hello")
+
+        e.assoc(use_original: true) # Access without creating
         e.save!
-        expect(SIS::EmployeeData.count).to eq 0
+        expect(AssociationTestClass.count).to eq 0
       end
     end
   end
